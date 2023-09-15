2976c5b107437e99b7fa8f16c71b00b765dcf617
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2023-09-15 07:01:41 -0400

    Updated exercise

--- scala2book/src/main/scala/Main.scala
+++ scala2book/src/main/scala/Main.scala
@@ -1,3 +1,21 @@
 object Main extends App {
-  println("Hello, World!")
+
+  abstract class Expr
+  case class Var(name: String) extends Expr
+  case class Number(num: Double) extends Expr
+  case class UnOp(operator: String, arg: Expr) extends Expr
+  case class BinOp(operator: String, left: Expr, right: Expr) extends Expr
+
+  def simplifyTop(expr: Expr): Expr = expr match {
+    case UnOp("-", UnOp("-", e))  => e // Double negation
+    case BinOp("+", e, Number(0)) => e // Adding zero
+    case BinOp("*", e, Number(1)) => e // Multiplying by one
+    case _                        => expr
+  }
+
+  val v = Var("x")
+  val op = BinOp("+", Number(1), v)
+
+  simplifyTop(UnOp("-", UnOp("-", Var("x"))))
+
 }
