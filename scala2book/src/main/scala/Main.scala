fb18dab61d92eb42e6be7ac25ddc64c21e325827
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2023-09-15 09:15:24 -0400

    Added Expression Formatter

--- scala2book/src/main/scala/Main.scala
+++ scala2book/src/main/scala/Main.scala
@@ -1,21 +1,26 @@
-object Main extends App {
+import org.stairwaybook.layout.Element
+import Element.elem
 
-  abstract class Expr
-  case class Var(name: String) extends Expr
-  case class Number(num: Double) extends Expr
-  case class UnOp(operator: String, arg: Expr) extends Expr
-  case class BinOp(operator: String, left: Expr, right: Expr) extends Expr
+import org.stairwaybook.expr._
 
-  def simplifyTop(expr: Expr): Expr = expr match {
-    case UnOp("-", UnOp("-", e))  => e // Double negation
-    case BinOp("+", e, Number(0)) => e // Adding zero
-    case BinOp("*", e, Number(1)) => e // Multiplying by one
-    case _                        => expr
-  }
+object Express extends App {
 
-  val v = Var("x")
-  val op = BinOp("+", Number(1), v)
+  val f = new ExprFormatter
 
-  simplifyTop(UnOp("-", UnOp("-", Var("x"))))
+  val e1 = BinOp(
+    "*",
+    BinOp("/", Number(1), Number(2)),
+    BinOp("+", Var("x"), Number(1))
+  )
+  val e2 = BinOp(
+    "+",
+    BinOp("/", Var("x"), Number(2)),
+    BinOp("/", Number(1.5), Var("x"))
+  )
+  val e3 = BinOp("/", e1, e2)
+
+  def show(e: Expr) = println(f.format(e) + "\n\n")
+
+  for (e <- Array(e1, e2, e3)) show(e)
 
 }
