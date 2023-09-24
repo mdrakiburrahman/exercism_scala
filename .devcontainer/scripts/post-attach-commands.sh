c4c73fe19291d5c9aae954a228d57f20c05a1190
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2023-09-24 19:18:42 -0400

    Spark initialization scripts

--- .devcontainer/scripts/post-attach-commands.sh
+++ .devcontainer/scripts/post-attach-commands.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -xe
 
+echo "Starting Apache Spark..."
+
+sudo /opt/spark/sbin/start-master.sh
+sudo /opt/spark/sbin/start-worker.sh spark://$(hostname):7077
+
+echo "Master UI: http://localhost:8080/"
+echo "Workers UI: http://localhost:8081/"
+
 echo "Post-Attach Commands Complete!"
