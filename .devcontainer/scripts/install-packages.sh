c7873cd534aca451257ded96ef46f05eca5d373c
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2023-09-25 07:43:42 -0400

    Don't need spark install

--- .devcontainer/scripts/install-packages.sh
+++ .devcontainer/scripts/install-packages.sh
@@ -14,22 +14,13 @@ apt-get install -y \
     lsb-release \
     software-properties-common \
     vim \
-    wget \
-    openjdk-8-jdk
+    wget
 
 echo "Installing yq..."
 VERSION=v4.25.1
 BINARY=yq_linux_386
 sudo wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY} -O /usr/bin/yq \
     && sudo chmod +x /usr/bin/yq
-
-echo "Installing Apache Spark..."
-sudo wget https://archive.apache.org/dist/spark/spark-3.3.1/spark-3.3.1-bin-hadoop3.tgz && \
-    tar -xvf spark-3.3.1-bin-hadoop3.tgz && \
-    sudo mkdir /opt/spark && \
-    sudo mv spark-3.3.1-bin-hadoop3/* /opt/spark && \
-    rm spark-3.3.1-bin-hadoop3.tgz && \
-    rm -rf spark-3.3.1-bin-hadoop3
 
 # Clean up
 sudo apt-get autoremove -y \
--- .devcontainer/scripts/post-attach-commands.sh
+++ .devcontainer/scripts/post-attach-commands.sh
@@ -1,27 +1,3 @@
-#!/bin/bash -e
+#!/bin/bash -xe
 
-echo "Starting Apache Spark..."
-
-# Check if Spark Master is running
-#
-if ! ps -ef | grep -q "org.apache.spark.deploy.master.Master"; then
-    echo "Spark is not running. Starting..."
-    sudo /opt/spark/sbin/start-master.sh
-    if ! ps -ef | grep -q "org.apache.spark.deploy.worker.Worker"; then
-        echo "Spark Worker is not running. Starting..."
-        sudo /opt/spark/sbin/start-worker.sh spark://$(hostname):7077
-    else
-        echo "Spark Worker already running"
-    fi
-else
-    echo "Spark Master already running"
-fi
-
-echo
-echo "----------------------------------"
-echo "Master UI:  http://localhost:8080"
-echo "Workers UI: http://localhost:8081"
-echo "----------------------------------"
-echo
 echo "Post-Attach Commands Complete!"
-echo
